5e524d0e1cabb8fdfadeb6f63bbb600b809d1dcb
Aurelien Pillevesse <Aurelien Pillevesse>
2017-04-22 14:47:37 +0200

    add request database for stock

--- src/main/java/io/github/aurelienpillevesse/MyResource.java
+++ src/main/java/io/github/aurelienpillevesse/MyResource.java
@@ -1,12 +1,23 @@
 package io.github.aurelienpillevesse;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
- * Root resource (exposed at "myresource" path)
+ * Root resource (exposed at "bookStock" path)
  */
 @Path("bookStock")
 public class MyResource {
@@ -19,7 +30,37 @@ public class MyResource {
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getStock() {
-        return "getStock()";
+    public String getStock(
+    	@QueryParam("isbn") int isbn
+    ) {
+    	//database connexion
+    	//get request : stock where isbn = :isbn
+    	
+    	ResultSet rs = null;
+    	PreparedStatement st = null;
+    	int stock = 0;
+    	
+    	try {		
+			st = getConnection().prepareStatement("select * from books where isbn = ?");
+			st.setInt(1, isbn); 
+			rs = st.executeQuery();
+			while (rs.next()) {
+				stock = rs.getInt("stock");
+			}
+			rs.close();
+			st.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	return "stock de livre pour l'isbn " + isbn + ": " + stock;
+    	
+    	//verification si isbn faux
+        
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        return DriverManager.getConnection(dbUrl);
     }
 }
