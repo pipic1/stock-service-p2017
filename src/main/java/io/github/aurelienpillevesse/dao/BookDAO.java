c778a7f3a11c4f03203f422dde77d96e3781d478
Aurelio <aurelienpillevesse@hotmail.fr>
2017-05-03 13:13:27 +0200

    update

--- src/main/java/io/github/aurelienpillevesse/dao/BookDAO.java
+++ src/main/java/io/github/aurelienpillevesse/dao/BookDAO.java
@@ -26,16 +26,16 @@ public class BookDAO extends DAO<Book> {
 		try {
 			this.st = this.connect.prepareStatement("select * from books where isbn = ?");
 			this.st.setString(1, isbn);
-			book.setIsbn(this.st.toString() + "haha");
-			/*this.rs = this.st.executeQuery();
+			book.setIsbn("haha");
+			this.rs = this.st.executeQuery();
 			while (rs.next()) {
-				book.setIsbn(this.rs.getString("isbn"));
-				book.setStock(this.rs.getInt("stock"));
-			}*/
+				//book.setIsbn(this.rs.getString("isbn"));
+				//book.setStock(this.rs.getInt("stock"));
+			}
 			this.rs.close();
 			this.st.close();
 		} catch (Exception e) {
-			book.setIsbn(e.getMessage());
+			book.setIsbn(e.toString());
 			e.printStackTrace();
 		}
 		
