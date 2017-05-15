8f8035136de244fd7033a607091d7f189b78833e
Aurelien Pillevesse <Aurelien Pillevesse>
2017-05-15 15:59:35 +0200

    add method update

--- src/main/java/io/github/aurelienpillevesse/dao/BookDAO.java
+++ src/main/java/io/github/aurelienpillevesse/dao/BookDAO.java
@@ -1,6 +1,9 @@
 package io.github.aurelienpillevesse.dao;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.github.aurelienpillevesse.model.Book;
 
 public class BookDAO extends DAO<Book> {
@@ -16,8 +19,45 @@ public class BookDAO extends DAO<Book> {
 		return false;
 	}
 
-	public boolean update(Book object) {
-		return false;
+	public void updateStock(Book book) {		
+		try {
+			this.st = this.connect.prepareStatement("update books set stock = ? where isbn = ?");
+			this.st.setString(1, book.getIsbn());
+			this.st.setInt(2, book.getStock());
+			this.st.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		    try { this.st.close(); } catch (Exception e) { /* ignored */ }
+		    try { this.connect.close(); } catch (Exception e) { /* ignored */ }
+		}
+		
+		return;
+	}
+	
+	public List<Book> findAll() {
+		List<Book> books = new ArrayList<>();
+		
+		try {
+			this.st = this.connect.prepareStatement("select * from books");
+			this.rs = this.st.executeQuery();
+			while (rs.next()) {
+				Book book = new Book();
+				
+				book.setIsbn(this.rs.getString("isbn"));
+				book.setBookName(this.rs.getString("book_name"));
+				
+				books.add(book);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		    try { this.st.close(); } catch (Exception e) { /* ignored */ }
+		    try { this.rs.close(); } catch (Exception e) { /* ignored */ }
+		    try { this.connect.close(); } catch (Exception e) { /* ignored */ }
+		}
+		
+		return books;
 	}
 
 	public Book find(String isbn) {
@@ -29,8 +69,9 @@ public class BookDAO extends DAO<Book> {
 			this.rs = this.st.executeQuery();
 			while (rs.next()) {
 				book.setIsbn(this.rs.getString("isbn"));
+				book.setBookName(this.rs.getString("book_name"));
 				book.setStock(this.rs.getInt("stock"));
-			}
+			}			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
--- src/main/java/io/github/aurelienpillevesse/dao/DAO.java
+++ src/main/java/io/github/aurelienpillevesse/dao/DAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public abstract class DAO<T> {
     protected Connection connect = null;
@@ -40,7 +41,7 @@ public abstract class DAO<T> {
     * @param object
     * @return boolean
     */
-    public abstract boolean update(T object);
+    public abstract void updateStock(T object);
 
     /**
     * Search by isbn
@@ -48,4 +49,10 @@ public abstract class DAO<T> {
     * @return T
     */
     public abstract T find(String isbn);
+    
+    /**
+     * Search all books
+     * @return T
+     */
+     public abstract List<T> findAll();
 }
